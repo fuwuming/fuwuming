d220b52d07eccc5c3104ceb4e965bb379641310a
itcast <boss@itcast.cn>
2025-04-19 14:39:57 +0800

    修改Demo01

---
+++ src/main/fuwuming/GitTestDemo01/gitStudy01.java
@@ -0,0 +1,13 @@
+package main.fuwuming.GitTestDemo01;
+
+/**
+ * 功能描述：
+ *
+ * @author: fuwuming
+ * @date: 2025/4/19 14:37
+ */
+public class gitStudy01 {
+    public static void main(String[] args) {
+        System.out.println("git..........");
+    }
+}
